ff0e5fbf279c9268c5a70a0ac3ea60f2649d9997
Carrolline <40234448+Carrolline@users.noreply.github.com>
2020-09-27 00:52:23 +0300

    Update compgraph_lab1_2.py

--- compgraph_lab1/compgraph_lab1_2.py
+++ compgraph_lab1/compgraph_lab1_2.py
@@ -153,7 +153,10 @@ def task3_1_def():
             Point(x * 50 + 100, y * 50 + 100).draw(window3_1)
             points1.append(tuple([x * 50 + 100, y * 50 + 100]))
         for i in range(1, len(points1)):
-            Line(Point(points1[i-1][0], points1[i-1][1]), Point(points1[i][0], points1[i][1])).draw(window3_1)
+            obj = Line(Point(points1[i-1][0], points1[i-1][1]), Point(points1[i][0], points1[i][1]))
+            obj.setOutline("green")
+            obj.setWidth(2)
+            obj.draw(window3_1)
         print(points1)
     else:
         window3_1.destroy()
@@ -173,24 +176,70 @@ def task3_2_def():
         Line(Point(415, 3), Point(425, 13)).draw(window3_2)
         Line(Point(415, 3), Point(405, 13)).draw(window3_2)
         a = 26
-        points1 = []
+        points2 = []
         for x in range(100):
             y = 3 * 0.01 * 26 * math.cos(x)
             Point(x * 50 + 100, -y * 50 + 100).draw(window3_2)
-            points1.append(tuple([x * 50 + 100, -y * 50 + 100]))
-        for i in range(1, len(points1)):
-            Line(Point(points1[i-1][0], points1[i-1][1]), Point(points1[i][0], points1[i][1])).draw(window3_2)
-        print(points1)
+            points2.append(tuple([x * 50 + 100, -y * 50 + 100]))
+        for i in range(1, len(points2)):
+            obj = Line(Point(points2[i-1][0], points2[i-1][1]), Point(points2[i][0], points2[i][1]))
+            obj.setOutline("deepskyblue")
+            obj.setWidth(2)
+            obj.draw(window3_2)
+        print(points2)
     else:
         window3_2.destroy()
         window3_2 = None
+
+
+def task3_4_def():
+    global window3_4
+    if window3_4 == None:
+        window3_4 = GraphWin("Вікно для графіки", 700, 200)
+        Line(Point(100, 100), Point(697, 100)).draw(window3_4)
+        Line(Point(415, 200), Point(415, 3)).draw(window3_4)
+        # arrow right
+        Line(Point(697, 100), Point(687, 90)).draw(window3_4)
+        Line(Point(697, 100), Point(687, 110)).draw(window3_4)
+        # arrow top
+        Line(Point(415, 3), Point(425, 13)).draw(window3_4)
+        Line(Point(415, 3), Point(405, 13)).draw(window3_4)
+        a = 26
+        points1 = []
+        for x in range(100):
+            y = 3 * 0.01 * 26 * math.sin(-x)
+            Point(x * 50 + 100, y * 50 + 100).draw(window3_4)
+            points1.append(tuple([x * 50 + 100, y * 50 + 100]))
+        for i in range(1, len(points1)):
+            obj = Line(Point(points1[i - 1][0], points1[i - 1][1]), Point(points1[i][0], points1[i][1]))
+            obj.setOutline("green")
+            obj.setWidth(2)
+            obj.draw(window3_4)
+        print(points1)
+
+        points2 = []
+        for x in range(100):
+            y = 3 * 0.01 * 26 * math.cos(x)
+            Point(x * 50 + 100, -y * 50 + 100).draw(window3_4)
+            points2.append(tuple([x * 50 + 100, -y * 50 + 100]))
+        for i in range(1, len(points2)):
+            obj = Line(Point(points2[i-1][0], points2[i-1][1]), Point(points2[i][0], points2[i][1]))
+            obj.setOutline("deepskyblue")
+            obj.setWidth(2)
+            obj.draw(window3_4)
+        print(points2)
+    else:
+        window3_4.destroy()
+        window3_4 = None
 
 
 task1_button = Button(root, text="Завдання 1", command=task1_def)
 task2_1_button = Button(root, text="Завдання 2.1", command=task2_1_def)
 task2_2_button = Button(root, text="Завдання 2.2", command=task2_2_def)
 task3_1_button = Button(root, text="Завдання 3.1", command=task3_1_def)
-task3_2_button = Button(root, text="Завдання 3.1", command=task3_2_def)
+task3_2_button = Button(root, text="Завдання 3.2", command=task3_2_def)
+
+task3_4_button = Button(root, text="Завдання 3.4", command=task3_4_def)
 
 window1 = None
 window2_1 = None
@@ -198,9 +247,13 @@ window2_2 = None
 window3_1 = None
 window3_2 = None
 
+window3_4 = None
+
 task1_button.pack()
 task2_1_button.pack()
 task2_2_button.pack()
 task3_1_button.pack()
 task3_2_button.pack()
+
+task3_4_button.pack()
 root.mainloop()
