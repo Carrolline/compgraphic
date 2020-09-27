fc5bc85381c9a73b115a381d8ad5977a44a4a577
Carrolline <40234448+Carrolline@users.noreply.github.com>
2020-09-27 12:45:52 +0300

    Update compgraph_lab1_2.py

--- compgraph_lab1/compgraph_lab1_2.py
+++ compgraph_lab1/compgraph_lab1_2.py
@@ -147,17 +147,23 @@ def task3_1_def():
         Line(Point(415, 3), Point(425, 13)).draw(window3_1)
         Line(Point(415, 3), Point(405, 13)).draw(window3_1)
         a = 26
+        f1 = 0
         points1 = []
-        for x in range(100):
+        points1_1 = []
+        while f1 < 10 * math.pi / 2:
+            points1.append(f1)
+            f1 += 0.1
+
+        for x in points1:
             y = 3 * 0.01 * 26 * math.sin(-x)
-            Point(x * 50 + 100, y * 50 + 100).draw(window3_1)
-            points1.append(tuple([x * 50 + 100, y * 50 + 100]))
-        for i in range(1, len(points1)):
-            obj = Line(Point(points1[i-1][0], points1[i-1][1]), Point(points1[i][0], points1[i][1]))
-            obj.setOutline("green")
+            points1_1.append(tuple([x * 50 + 100, y * 50 + 100]))
+        for i in range(1, len(points1_1)):
+            obj = Line(Point(points1_1[i - 1][0], points1_1[i - 1][1]), Point(points1_1[i][0], points1_1[i][1]))
+            obj.setOutline("deepskyblue")
             obj.setWidth(2)
             obj.draw(window3_1)
-        print(points1)
+
+
     else:
         window3_1.destroy()
         window3_1 = None
@@ -175,21 +181,110 @@ def task3_2_def():
         # arrow top
         Line(Point(415, 3), Point(425, 13)).draw(window3_2)
         Line(Point(415, 3), Point(405, 13)).draw(window3_2)
+
         a = 26
+        f2 = 0
         points2 = []
-        for x in range(100):
+        points2_1 = []
+        while f2 < 10*math.pi / 2:
+            points2.append(f2)
+            f2 += 0.1
+
+        for x in points2:
             y = 3 * 0.01 * 26 * math.cos(x)
-            Point(x * 50 + 100, -y * 50 + 100).draw(window3_2)
-            points2.append(tuple([x * 50 + 100, -y * 50 + 100]))
-        for i in range(1, len(points2)):
-            obj = Line(Point(points2[i-1][0], points2[i-1][1]), Point(points2[i][0], points2[i][1]))
-            obj.setOutline("deepskyblue")
+            points2_1.append(tuple([x * 50 + 100, -y * 50 + 100]))
+        for i in range(1, len(points2_1)):
+            obj = Line(Point(points2_1[i-1][0], points2_1[i-1][1]), Point(points2_1[i][0], points2_1[i][1]))
+            obj.setOutline("lime")
             obj.setWidth(2)
             obj.draw(window3_2)
-        print(points2)
+        print(points2_1)
     else:
         window3_2.destroy()
         window3_2 = None
+
+
+def task3_3_def():
+    global window3_3
+    if window3_3 == None:
+        window3_3 = GraphWin("Вікно для графіки", 700, 200)
+        Line(Point(100, 100), Point(697, 100)).draw(window3_3)
+        Line(Point(415, 200), Point(415, 3)).draw(window3_3)
+        # arrow right
+        Line(Point(697, 100), Point(687, 90)).draw(window3_3)
+        Line(Point(697, 100), Point(687, 110)).draw(window3_3)
+        # arrow top
+        Line(Point(415, 3), Point(425, 13)).draw(window3_3)
+        Line(Point(415, 3), Point(405, 13)).draw(window3_3)
+        a = 26
+        points2 = []
+        ################################
+        a = 26
+        points3_1 = []
+        points3_2 = []
+        points3_3 = []
+        points3_4 = []
+        list1 = []
+        list2 = []
+        list3 = []
+        list4 = []
+        f1 = 0
+        while f1 < math.pi / 2:
+            list1.append(f1)
+            f1 += 0.1
+        for x in list1:
+            y = 3 * 0.01 * 26 * math.tan(-x)
+            points3_1.append(tuple([x * 50 + 100, y * 50 + 100]))
+        for i in range(1, len(points3_1)):
+            obj = Line(Point(points3_1[i - 1][0], points3_1[i - 1][1]), Point(points3_1[i][0], points3_1[i][1]))
+            obj.setOutline("red")
+            obj.setWidth(2)
+            obj.draw(window3_3)
+
+        f2 = math.pi / 2 + 0.1
+        while f2 < 3 * math.pi / 2:
+            list2.append(f2)
+            f2 += 0.1
+        for x in list2:
+            y = 3 * 0.01 * 26 * math.tan(-x)
+            points3_2.append(tuple([x * 50 + 100, y * 50 + 100]))
+        for i in range(1, len(points3_2)):
+            obj = Line(Point(points3_2[i - 1][0], points3_2[i - 1][1]), Point(points3_2[i][0], points3_2[i][1]))
+            obj.setOutline("red")
+            obj.setWidth(2)
+            obj.draw(window3_3)
+
+        f3 = 3 * math.pi / 2 + 0.1
+        while f3 < 5 * math.pi / 2:
+            list3.append(f3)
+            f3 += 0.1
+        for x in list3:
+            y = 3 * 0.01 * 26 * math.tan(-x)
+            points3_3.append(tuple([x * 50 + 100, y * 50 + 100]))
+        for i in range(1, len(points3_3)):
+            obj = Line(Point(points3_3[i - 1][0], points3_3[i - 1][1]), Point(points3_3[i][0], points3_3[i][1]))
+            obj.setOutline("red")
+            obj.setWidth(2)
+            obj.draw(window3_3)
+
+        f4 = 5 * math.pi / 2 + 0.1
+        while f4 < 7 * math.pi / 2:
+            list4.append(f4)
+            f4 += 0.1
+        for x in list4:
+            y = 3 * 0.01 * 26 * math.tan(-x)
+            points3_4.append(tuple([x * 50 + 100, y * 50 + 100]))
+        for i in range(1, len(points3_4)):
+            obj = Line(Point(points3_4[i - 1][0], points3_4[i - 1][1]), Point(points3_4[i][0], points3_4[i][1]))
+            obj.setOutline("red")
+            obj.setWidth(2)
+            obj.draw(window3_3)
+
+        #################################
+
+    else:
+        window3_3.destroy()
+        window3_3 = None
 
 
 def task3_4_def():
@@ -205,29 +300,103 @@ def task3_4_def():
         Line(Point(415, 3), Point(425, 13)).draw(window3_4)
         Line(Point(415, 3), Point(405, 13)).draw(window3_4)
         a = 26
+        f1 = 0
         points1 = []
-        for x in range(100):
-            y = 3 * 0.01 * 26 * math.sin(-x)
-            Point(x * 50 + 100, y * 50 + 100).draw(window3_4)
-            points1.append(tuple([x * 50 + 100, y * 50 + 100]))
-        for i in range(1, len(points1)):
-            obj = Line(Point(points1[i - 1][0], points1[i - 1][1]), Point(points1[i][0], points1[i][1]))
-            obj.setOutline("green")
-            obj.setWidth(2)
-            obj.draw(window3_4)
-        print(points1)
+        points1_1 = []
+        while f1 < 10 * math.pi / 2:
+            points1.append(f1)
+            f1 += 0.1
 
-        points2 = []
-        for x in range(100):
-            y = 3 * 0.01 * 26 * math.cos(x)
-            Point(x * 50 + 100, -y * 50 + 100).draw(window3_4)
-            points2.append(tuple([x * 50 + 100, -y * 50 + 100]))
-        for i in range(1, len(points2)):
-            obj = Line(Point(points2[i-1][0], points2[i-1][1]), Point(points2[i][0], points2[i][1]))
+        for x in points1:
+            y = 3 * 0.01 * 26 * math.sin(-x)
+            points1_1.append(tuple([x * 50 + 100, y * 50 + 100]))
+        for i in range(1, len(points1_1)):
+            obj = Line(Point(points1_1[i - 1][0], points1_1[i - 1][1]), Point(points1_1[i][0], points1_1[i][1]))
             obj.setOutline("deepskyblue")
             obj.setWidth(2)
             obj.draw(window3_4)
-        print(points2)
+
+        a = 26
+        f2 = 0
+        points2 = []
+        points2_1 = []
+        while f2 < 10 * math.pi / 2:
+            points2.append(f2)
+            f2 += 0.1
+
+        for x in points2:
+            y = 3 * 0.01 * 26 * math.cos(x)
+            points2_1.append(tuple([x * 50 + 100, -y * 50 + 100]))
+        for i in range(1, len(points2_1)):
+            obj = Line(Point(points2_1[i - 1][0], points2_1[i - 1][1]), Point(points2_1[i][0], points2_1[i][1]))
+            obj.setOutline("lime")
+            obj.setWidth(2)
+            obj.draw(window3_4)
+        print(points2_1)
+
+        ######################################
+
+        a = 26
+        points3_1 = []
+        points3_2 = []
+        points3_3 = []
+        points3_4 = []
+        list1 = []
+        list2 = []
+        list3 = []
+        list4 = []
+        f1_4 = 0
+        while f1_4 < math.pi / 2:
+            list1.append(f1_4)
+            f1_4 += 0.1
+        for x in list1:
+            y = 3 * 0.01 * 26 * math.tan(-x)
+            points3_1.append(tuple([x * 50 + 100, y * 50 + 100]))
+        for i in range(1, len(points3_1)):
+            obj = Line(Point(points3_1[i - 1][0], points3_1[i - 1][1]), Point(points3_1[i][0], points3_1[i][1]))
+            obj.setOutline("red")
+            obj.setWidth(2)
+            obj.draw(window3_4)
+
+        f2_4 = math.pi / 2 + 0.1
+        while f2_4 < 3 * math.pi / 2:
+            list2.append(f2_4)
+            f2_4 += 0.1
+        for x in list2:
+            y = 3 * 0.01 * 26 * math.tan(-x)
+            points3_2.append(tuple([x * 50 + 100, y * 50 + 100]))
+        for i in range(1, len(points3_2)):
+            obj = Line(Point(points3_2[i - 1][0], points3_2[i - 1][1]), Point(points3_2[i][0], points3_2[i][1]))
+            obj.setOutline("red")
+            obj.setWidth(2)
+            obj.draw(window3_4)
+
+        f3_3 = 3 * math.pi / 2 + 0.1
+        while f3_3 < 5 * math.pi / 2:
+            list3.append(f3_3)
+            f3_3 += 0.1
+        for x in list3:
+            y = 3 * 0.01 * 26 * math.tan(-x)
+            points3_3.append(tuple([x * 50 + 100, y * 50 + 100]))
+        for i in range(1, len(points3_3)):
+            obj = Line(Point(points3_3[i - 1][0], points3_3[i - 1][1]), Point(points3_3[i][0], points3_3[i][1]))
+            obj.setOutline("red")
+            obj.setWidth(2)
+            obj.draw(window3_4)
+
+        f4_4 = 5 * math.pi / 2 + 0.1
+        while f4_4 < 7 * math.pi / 2:
+            list4.append(f4_4)
+            f4_4 += 0.1
+        for x in list4:
+            y = 3 * 0.01 * 26 * math.tan(-x)
+            points3_4.append(tuple([x * 50 + 100, y * 50 + 100]))
+        for i in range(1, len(points3_4)):
+            obj = Line(Point(points3_4[i - 1][0], points3_4[i - 1][1]), Point(points3_4[i][0], points3_4[i][1]))
+            obj.setOutline("red")
+            obj.setWidth(2)
+            obj.draw(window3_4)
+
     else:
         window3_4.destroy()
         window3_4 = None
@@ -238,7 +407,7 @@ task2_1_button = Button(root, text="Завдання 2.1", command=task2_1_def)
 task2_2_button = Button(root, text="Завдання 2.2", command=task2_2_def)
 task3_1_button = Button(root, text="Завдання 3.1", command=task3_1_def)
 task3_2_button = Button(root, text="Завдання 3.2", command=task3_2_def)
-
+task3_3_button = Button(root, text="Завдання 3.3", command=task3_3_def)
 task3_4_button = Button(root, text="Завдання 3.4", command=task3_4_def)
 
 window1 = None
@@ -246,7 +415,7 @@ window2_1 = None
 window2_2 = None
 window3_1 = None
 window3_2 = None
-
+window3_3 = None
 window3_4 = None
 
 task1_button.pack()
@@ -254,6 +423,6 @@ task2_1_button.pack()
 task2_2_button.pack()
 task3_1_button.pack()
 task3_2_button.pack()
-
+task3_3_button.pack()
 task3_4_button.pack()
 root.mainloop()
